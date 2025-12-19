7727ecc32f7d40633b6fe391041f76cf0e5f48c5
askardurzhanov <durzanovaskar0@gmail.com>
2025-12-19 21:28:08 +0500

    Implemented assignment 1

---
+++ src/models/Point.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Point {
+    private double x;
+    private double y;
+
+    // constructor having both parameters
+
+    // getters and setters
+
+    // distance(Point dest) - distance from current point to the dest point
+
+    // toString
+}
