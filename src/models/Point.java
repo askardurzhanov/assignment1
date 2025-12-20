263490f97408a92b74bdd2633eb16b236138375e
askardurzhanov <durzanovaskar0@gmail.com>
2025-12-20 21:17:28 +0500

    constructor

--- src/models/Point.java
+++ src/models/Point.java
@@ -5,7 +5,10 @@ public class Point {
     private double y;
 
     // constructor having both parameters
-
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
     // getters and setters
 
     // distance(Point dest) - distance from current point to the dest point
