896e1b410f4f2f1543c95985d178eaf906784583
askardurzhanov <durzanovaskar0@gmail.com>
2025-12-20 23:02:44 +0500

    Changes Point class

--- src/models/Point.java
+++ src/models/Point.java
@@ -4,12 +4,11 @@ public class Point {
     private double x;
     private double y;
 
-    // constructor having both parameters
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
-    // getters and setters
+
     public double getX() {
         return x;
     }
@@ -17,13 +16,13 @@ public class Point {
     public double getY() {
         return y;
     }
-    // distance(Point dest) - distance from current point to the dest point
+
 public double distance(Point dest) {
         double dx = this.x - dest.x;
         double dy = this.y - dest.y;
         return Math.sqrt(dx * dx + dy * dy);
 }
-    // toString
+
 public String toString() {
     return "(" + x + "," + y + ")";
     }
